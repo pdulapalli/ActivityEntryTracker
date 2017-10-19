715ebee6e45923dc3d693029475f2ef16f9c416c
Anurag Dulapalli <pdulapalli@gmail.com>
2017-10-19 17:00:35 -0400

    Initial Commit

---
+++ FitnessDataTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitnessDataTracker
+//
+//  Created by Anurag Dulapalli on 10/19/17.
+//  Copyright © 2017 Anurag Dulapalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
